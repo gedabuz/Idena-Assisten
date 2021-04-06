8430f28a17dc808290e23a63043e210aa08860ed
gedabuz <30450582+gedabuz@users.noreply.github.com>
2021-04-06 23:41:49 +0700

    Installation Patch update

--- idena-assis-instal.sh
+++ idena-assis-instal.sh
@@ -1,1 +1,2 @@
-wget https://raw.githubusercontent.com/gedabuz/Idena-Assisten/main/idena-assis.sh && chmod +x idena-assis.sh && sed -i 's/\r//' idena-assis.sh
+wget https://raw.githubusercontent.com/gedabuz/Idena-Assisten/main/idena-assis.sh -q -O /usr/bin/idena-assis
+chmod +x /usr/bin/idena-assis
